b8cf392b4e8b2e303e237464f49a282c299630f8
alimelon <alimelon97@gmail.com>
2020-12-24 15:01:40 +0900

    [DFS/BFS] 타겟넘버

---
+++ Programmers/targetNumber.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+var inputNumbers = [Int](), targetNumber = 0
+var result = 0
+
+func solution(_ numbers: [Int], _ target: Int) -> Int {
+    inputNumbers = numbers
+    targetNumber = target
+    dfs(1, numbers[0])
+    dfs(1, -numbers[0])
+    return result
+}
+
+func dfs(_ i: Int, _ sum: Int) {
+    if i == inputNumbers.count {
+        if sum == targetNumber { result += 1 }
+    } else {
+        dfs(i+1, sum + inputNumbers[i])
+        dfs(i+1, sum - inputNumbers[i])
+    }
+}
