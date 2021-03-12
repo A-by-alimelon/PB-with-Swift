0453972ea3f22ec1ac853a16639c3dc5a8af228d
alimelon <alimelon97@gmail.com>
2021-03-12 17:51:25 +0900

    [최단경로] 다익스트라 간단한 버전 수정

--- practice/[최단경로]다익스트라_간단한버전.swift
+++ practice/[최단경로]다익스트라_간단한버전.swift
@@ -1,4 +1,4 @@
-// 간단한 다익스트라 알고리즘 구현
+/// 간단한 다익스트라 알고리즘 구현
 let inputs = readLine()!.split(separator: " ").map { return Int($0)! }
 // n = 노드 개수, m = 간선 개수
 let n = inputs[0], m = inputs[1]
@@ -11,9 +11,12 @@ var distance = Array(repeating: Int.max, count: n + 1)
 for _ in 0..<m {
     let values = readLine()!.split(separator: " ").map { return Int($0)! }
     let a = values[0], b = values[1], c = values[2]
+    
+    // a-b 사이의 거리 c 저장
     graph[a].append((b, c))
 }
 
+// 방문하지 않은 노드 중 최단 거리의 노드 번호 반환
 func getSmallestNode() -> Int {
     var min = Int.max
     var index = 0
@@ -27,12 +30,16 @@ func getSmallestNode() -> Int {
 }
 
 func dijkstra(_ start: Int) {
+    // 시작 노드에 대한 초기화
     distance[start] = 0
     visited[start] = true
+    
     for j in graph[start] {
         distance[j.0] = j.1
     }
-    for i in 0..<n-1 {
+    
+    // 시작 노드를 제외한 전체 노드에 대해 최단 거리 계산
+    for _ in 0..<n-1 {
         let now = getSmallestNode()
         visited[now] = true
         for j in graph[now] {
