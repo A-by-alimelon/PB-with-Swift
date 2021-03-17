d6a19e824c63c3d765a67f40dc00dcefda418b41
alimelon <alimelon97@gmail.com>
2021-03-18 01:41:45 +0900

    주식가격 수정

--- Programmers/주식가격.swift
+++ Programmers/주식가격.swift
@@ -6,16 +6,23 @@ func solution(_ prices: [Int]) -> [Int] {
     
     stack.forEach {
         array.append($0)
-        for (index, value) in array.reversed().enumerated() {
-            if value < $0 {
-                result.append(array.count - 1 - index)
+        print("array: \(array)")
+        for i in stride(from: array.count-1, to: 0, by: -1) {
+            print("value: \(array[i]), $0: \($0), index: \(i)")
+            if array[i] < $0 {
+                print(array.count - 1 - i)
+                result.append(array.count - 1 - i)
                 return
             }
+            
         }
         result.append(array.count - 1)
+        print("result: \(result)")
     }
     
     return result.reversed()
 }
 
+
 print(solution([1, 2, 3, 2, 3]))
+print(solution([5, 8, 6, 2, 4, 1]))
