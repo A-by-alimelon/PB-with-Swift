7d02d017c78c8c7459fa8ad65e330e8d7bc036c8
alimelon <alimelon97@gmail.com>
2021-05-04 22:32:03 +0900

    스택 사용 - 입력에서 시간초과

--- BOJ/1406.swift
+++ BOJ/1406.swift
@@ -1,91 +1,29 @@
 // BOJ 1406
-// 야매 연결 리스트 이용
-var max = 600005
-var data = Array(repeating: "", count: max)
-var pre = Array(repeating: -1, count: max)
-var nxt = Array(repeating: -1, count: max)
-var unused = 1;
-
-func insert(_ addr: Int, _ str: String) {
-    // 새로운 원소 생성 후 pre, nxt 지정
-    data[unused] = str
-    pre[unused] = addr
-    nxt[unused] = nxt[addr]
-    
-    if nxt[addr] != -1 {
-        pre[nxt[addr]] = unused
-    }
-    
-    nxt[addr] = unused
-    
-    unused += 1
-}
-
-// 메모리 누수 있지만 신경쓰지 않겠다.
-func erase(_ addr: Int) {
-    nxt[pre[addr]] = nxt[addr]
-    
-    if nxt[addr] != -1 {
-        pre[nxt[addr]] = pre[addr]
-    }
-}
-
-func traverse() {
-    var cur = nxt[0]
-    
-    while cur != -1 {
-        print(data[cur], terminator: " ")
-        cur = nxt[cur]
-    }
-    
-    print()
-}
-
-func printString() {
-    var cur = nxt[0]
-    
-    while cur != -1 {
-        print(data[cur], terminator: "")
-        cur = nxt[cur]
-    }
-    
-    print()
-}
-
-var string = Array(readLine()!)
-
-for i in 0..<string.count {
-    insert(i, String(string[i]))
-}
+var lStack = Array(readLine()!)
+var rStack = [Character]()
 
 let n = Int(readLine()!)!
-var cursor = string.count
-
 for _ in 0..<n {
     let command = readLine()!.split(separator: " ").map { return String($0) }
-    
-    switch command.first {
+    switch command[0] {
     case "L":
-        if pre[cursor] != -1 {
-            cursor = pre[cursor]
+        if !lStack.isEmpty {
+            rStack.append(lStack.removeLast())
         }
     case "D":
-        if nxt[cursor] != -1 {
-            cursor = nxt[cursor]
+        if !rStack.isEmpty {
+            lStack.append(rStack.removeLast())
+        }
+    case "B":
+        if !lStack.isEmpty {
+            lStack.removeLast()
         }
         
-    case "B":
-        if cursor != 0 {
-            erase(cursor)
-            cursor = pre[cursor]
-        }
     case "P":
-        insert(cursor, command[1])
-        cursor = nxt[cursor]
+        lStack.append(contentsOf: command[1])
     default:
         print()
     }
-
 }
 
-printString()
+print((lStack + rStack.reversed()).map{ return String($0) }.joined())
