e5c6459c844c6120e511ff69c236a19bd4e8988f
alimelon <alimelon97@gmail.com>
2021-05-06 15:26:53 +0900

    [bfs] 미로 탐색

--- BOJ/2178.swift
+++ BOJ/2178.swift
@@ -1,43 +1,33 @@
-//
-//  2178.swift
-//  
-//
-//  Created by A on 2020/09/15.
-//
-
-import Foundation
+// BOJ 2178
 let inputs = readLine()!.split(separator: " ").map { return Int($0)! }
 let n = inputs[0], m = inputs[1]
 
-var graph = [[Int]]()
+var map = [[Int]]()
+var dist = Array(repeating: Array(repeating: -1, count: m), count: n)
+
 for _ in 0..<n {
-    graph.append(Array(readLine()!).map { return Int(String($0))! })
+    map.append(Array(readLine()!).map { return Int(String($0))! })
 }
 
-var dx = [-1, 1, 0, 0]
-var dy = [0, 0, -1, 1]
+var queue = [(Int, Int)]()
+dist[0][0] = 1
+queue.append((0, 0))
+let dx = [0, 1, 0, -1]
+let dy = [1, 0, -1, 0]
 
-func bfs(_ x: Int, _ y: Int) -> Int {
-   var queue = [(Int, Int)]()
-    queue.append((x, y))
-    while !queue.isEmpty {
-        var (x, y) = queue.removeFirst()
-        for i in 0..<4 {
-            let nx = x + dx[i]
-            let ny = y + dy[i]
-            if nx < 0 || nx >= n || ny < 0 || ny >= m {
-                continue
-            }
-            if graph[nx][ny] == 0 {
-                continue
-            }
-            if graph[nx][ny] == 1 {
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-            }
-        }
+while !queue.isEmpty {
+    let cur = queue.removeFirst()
+    for i in 0..<4 {
+        let nx = cur.0 + dx[i]
+        let ny = cur.1 + dy[i]
+        
+        if nx < 0 || nx >= n || ny < 0 || ny >= m { continue }
+        if dist[nx][ny] != -1 || map[nx][ny] == 0 { continue }
+        
+        dist[nx][ny] = dist[cur.0][cur.1] + 1
+        queue.append((nx, ny))
     }
-    return graph[n-1][m-1]
 }
 
-print(bfs(0, 0))
+
+print(dist[n-1][m-1])
