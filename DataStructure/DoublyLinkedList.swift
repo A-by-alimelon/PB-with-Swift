97f6e3f076f7db32b445a9f2132eab3e03561aa2
alimelon <alimelon97@gmail.com>
2021-05-30 23:59:23 +0900

    삽입 연산 구현

--- DataStructure/DoublyLinkedList.swift
+++ DataStructure/DoublyLinkedList.swift
@@ -28,6 +28,33 @@ class LinkedList<T> {
             tail = newNode
         }
     }
-
     
+    func insert(at index: Int, _ value: T) {
+        let newNode = Node(value)
+        var current = head
+        
+        if isEmpty { // 링크드 리스트가 비어있으면
+            appendData(value)
+        } else { // 링크드 리스트가 비어있지 않으면
+            if index == 0 { // head에 삽입할 경우
+                current?.next = head
+                head?.prev = newNode
+                head = newNode
+            } else { // k번째에 삽입
+                for _ in 0..<index - 1 { // 삽입하고자 하는 위치의 바로 직전 노드를 찾는다.
+                    current = current?.next
+                    
+                    guard current != nil else { // k번째가 현재 노드의 개수보다 뒤일 경우
+                        appendData(value) // 맨 뒤에 삽입
+                        return
+                    }
+                }
+                
+                current?.next?.prev = newNode
+                newNode.next = current?.next
+                newNode.prev = current
+                current?.next = newNode
+            }
+        }
+    }
 }
