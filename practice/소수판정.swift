1873b2a3735cdcb3328c8750a18fc8b84928e59a
alimelon <alimelon97@gmail.com>
2021-06-02 23:20:32 +0900

    2, 3 일 때 예외 처리

--- practice/소수판정.swift
+++ practice/소수판정.swift
@@ -1,6 +1,10 @@
 func isPrime(_ num: Int) -> Bool {
     var isPrime = true
     
+    if num == 2 || num == 3 {
+        return true
+    }
+    
     for i in 2...Int(sqrt(Double(num))) {
         if num % i == 0 {
             isPrime = false
